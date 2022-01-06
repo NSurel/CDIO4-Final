47879213b470055c556319ef035bd0a292eef91c
Martin Wenzel <s203867@student.dtu.dk>
2022-01-06 12:33:31 +0100

    Added getters and setters for Deed

--- src/main/java/Deeds/Deed.java
+++ src/main/java/Deeds/Deed.java
@@ -1,4 +1,35 @@
 package Deeds;
 
 public abstract class Deed {
+    private int rent;
+    private int value;
+    private int owner;
+    private int type;
+
+
+
+    public int getRent() {
+        return rent;
+    }
+    public int getOwner() {
+        return owner;
+    }
+    public int getType() {
+        return type;
+    }
+    public int getValue() {
+        return value;
+    }
+    public void setRent(int rent) {
+        this.rent = rent;
+    }
+    public void setOwner(int owner) {
+        this.owner = owner;
+    }
+    public void setType(int type) {
+        this.type = type;
+    }
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
