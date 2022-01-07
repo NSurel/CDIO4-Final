318f754901d4da5dcfde61f1a1157fa595f9eced
Noah Grænge Surel <s215783@student.dtu.dk>
2022-01-07 14:20:50 +0100

    GuiController added

--- src/main/java/Controllers/GuiController.java
+++ src/main/java/Controllers/GuiController.java
@@ -1,4 +1,16 @@
 package Controllers;
+import gui_fields.GUI_Field;
+import gui_main.*;
 
 public class GuiController {
+    private final GUI gui;
+
+    public GuiController(){
+        gui = new GUI();
+        System.out.println(gui.getFields()[1].getSubText());
+    }
+
+    public void changePrice(int pos, int value){
+        gui.getFields()[pos].setSubText("pris: "+value);
+    }
 }
