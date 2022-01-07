61dd3bf4057106473bb492ed83edfa52bc497614
Noah Grænge Surel <s215783@student.dtu.dk>
2022-01-07 10:58:14 +0100

    Added some features for PlayerController

--- src/main/java/Controllers/PlayerController.java
+++ src/main/java/Controllers/PlayerController.java
@@ -4,4 +4,22 @@ import Models.Player;
 
 public class PlayerController {
     private Player[] players;
+
+    public PlayerController(){
+
+    }
+
+    public void createPlayers(int size){
+        players = new Player[size];
+        for (int i = 0; i < size; i++) {
+            players[i] = new Player("Player"+ (i + 1),i);
+        }
+    }
+
+    public Player[] getPlayers() {
+        return players;
+    }
+    public void updatePlayerBal(int value, Player player){
+        player.updateBalance(value);
+    }
 }
