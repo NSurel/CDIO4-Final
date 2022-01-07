63de11c0c9a8d28e8dcb20b1f55ddf789361eb31
Martin Wenzel <s203867@student.dtu.dk>
2022-01-07 14:29:21 +0100

    Made method able of calculating a players liqiud assets

--- src/main/java/Models/Deeds/Property.java
+++ src/main/java/Models/Deeds/Property.java
@@ -7,4 +7,14 @@ public class Property extends Deed{
     public Property(int rent, int value, int type){
         super( rent,value,type);
     }
+
+    @Override
+    public int getRent() {
+        return super.getRent();
+    }
+
+    @Override
+    public int getValue() {
+        return super.getValue();
+    }
 }
